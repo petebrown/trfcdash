17761c426ff89f9c6cde4bcd577bd7ecdc31eec6
petebrown <15838790+petebrown@users.noreply.github.com>
2023-07-05 09:10:40 -0400

    added first rda files

--- data-raw/players_dataset.R
+++ data-raw/players_dataset.R
@@ -1,5 +1,3 @@
-## code to prepare `players_dataset` dataset goes here
-
 fix_sb_player_names <- function(df) {
   df %>%
     dplyr::mutate(
@@ -38,15 +36,70 @@ fix_sb_game_ids <- function(df) {
 }
 
 
+comp_rec_goals <- vroom::vroom(
+  file = "https://raw.githubusercontent.com/petebrown/complete-record/main/output/cr-scorers.csv",
+  show_col_types = FALSE
+)
+
+sb_goals <- vroom::vroom(
+  file = "https://raw.githubusercontent.com/petebrown/scrape-goals/main/data/goals.csv",
+  col_select = c("game_id", "player_id", "player_name", "minute", "penalty", "own_goal", "goal_type"),
+  show_col_types = FALSE
+) %>%
+  fix_sb_player_names()
+
+goals <- vroom::vroom(
+  file = "https://raw.githubusercontent.com/petebrown/complete-record/main/output/scorers-long.csv",
+  show_col_types = FALSE
+)
+
 comp_rec_plr_seasons <- vroom::vroom(
   file = "https://raw.githubusercontent.com/petebrown/complete-record/main/output/player_ssns.csv",
   show_col_types = FALSE
 )
 
+game_dates_and_nos <- results_dataset %>%
+  dplyr::select(
+    season,
+    ssn_game_no,
+    game_date
+  )
 
 comp_rec_plr_apps <- vroom::vroom(
   file = "https://raw.githubusercontent.com/petebrown/complete-record/main/output/apps_long.csv",
-  show_col_types = FALSE)
+  show_col_types = FALSE) %>%
+  dplyr::left_join(
+    game_dates_and_nos,
+    by = c("season" = "season", "game_no" = "ssn_game_no")
+  ) %>%
+  dplyr::left_join(
+    comp_rec_plr_seasons %>%
+      dplyr::select(
+        season,
+        disam_name,
+        player_name
+      ) %>% unique(),
+    by = c("season" = "season", "player_name" = "disam_name")
+  ) %>%
+  dplyr::mutate(
+    player_name = player_name.y
+  ) %>%
+  dplyr::select(-player_name.y) %>%
+  dplyr::left_join(
+    goals,
+    by = c("season" = "season", "game_no" = "game_no", "player_name" = "player_name")
+  ) %>%
+  dplyr::select(
+    season,
+    game_no,
+    game_date,
+    player_name,
+    role,
+    goals_scored,
+    shirt_no,
+    on_for,
+    off_for
+  )
 
 
 squad_nos <- vroom::vroom(
@@ -58,6 +111,19 @@ sb_subs_and_reds <- vroom::vroom(
   file = "https://raw.githubusercontent.com/petebrown/scrape-events/main/data/subs-and-reds.csv",
   show_col_types = FALSE
 )
+
+game_lengths <- results_dataset %>%
+  dplyr::mutate(
+    game_length = dplyr::case_when(
+      !is.na(gg_outcome) ~ 116,
+      is.na(gg_outcome) & extra_time == 1 ~ 120,
+      .default = 90
+    )
+  ) %>%
+  dplyr::select(
+    game_date,
+    game_length
+  )
 
 sb_player_apps <- vroom::vroom(
   file = "https://raw.githubusercontent.com/petebrown/update-player-stats/main/data/players_df.csv",
@@ -90,11 +156,13 @@ sb_player_apps <- vroom::vroom(
     )
   ) %>%
   unique() %>%
-  dplyr::rename(shirt_no = squad_no) %>%
   dplyr::left_join(
-    x = sb_player_apps,
     y = sb_subs_and_reds,
     by = c("game_id", "player_id")
+  ) %>%
+  dplyr::left_join(
+    game_lengths,
+    by = "game_date"
   ) %>%
   dplyr::mutate(
     role = dplyr::case_when(
@@ -102,42 +170,47 @@ sb_player_apps <- vroom::vroom(
       !is.na(min_on) ~ "sub"
     ),
     mins_played = dplyr::case_when(
-      role == "starter" & is.na(min_off) & is.na(min_so) ~ 90,
+      role == "starter" & is.na(min_off) & is.na(min_so) ~ game_length,
       role == "starter" & !is.na(min_off) ~ min_off,
       role == "starter" & !is.na(min_so) ~ min_so,
-      role == "sub" & is.na(min_off) & is.na(min_so) ~ 90 - min_on,
+      role == "sub" & is.na(min_off) & is.na(min_so) ~ game_length - min_on,
       role == "sub" & is.na(min_off) ~ min_off - min_on,
       role == "sub" & is.na(min_so) ~ min_so - min_on
     )
+  ) %>%
+  dplyr::select(
+    season,
+    game_date,
+    player_name,
+    role,
+    pl_goals,
+    squad_no,
+    mins_played,
+    yellow_cards,
+    red_cards
+  ) %>%
+  dplyr::rename(
+    goals_scored = pl_goals,
+    shirt_no = squad_no
   )
 
-comp_rec_goals <- vroom::vroom(
-  file = "https://raw.githubusercontent.com/petebrown/complete-record/main/output/cr-scorers.csv",
-  show_col_types = FALSE
-)
-
-sb_goals <- vroom::vroom(
-  file = "https://raw.githubusercontent.com/petebrown/scrape-goals/main/data/goals.csv",
-  col_select = c("game_id", "player_id", "player_name", "minute", "penalty", "own_goal", "goal_type"),
-  show_col_types = FALSE
+player_apps <- dplyr::bind_rows(
+  comp_rec_plr_apps,
+  sb_player_apps
 ) %>%
-  fix_sb_player_names()
-
-goals <- vroom::vroom(
-  file = "https://raw.githubusercontent.com/petebrown/complete-record/main/output/scorers-long.csv",
-  show_col_types = FALSE
-)
-
-goals_2 <- goals %>%
-  dplyr::group_by(
+  dplyr::select(
     season,
     game_no,
-    player_name
-  ) %>%
-  dplyr::summarise(
-    goals_scored = sum(goals_scored),
-    .groups = "drop"
+    game_date,
+    player_name,
+    role,
+    goals_scored,
+    mins_played,
+    yellow_cards,
+    red_cards,
+    shirt_no,
+    on_for,
+    off_for,
   )
 
-
-usethis::use_data(players_dataset, overwrite = TRUE)
+usethis::use_data(player_apps, overwrite = TRUE)
