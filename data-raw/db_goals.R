bd1ed8c64448fa13ff1c99304e8a5fe04e1f8255
Pete Brown <15838790+petebrown@users.noreply.github.com>
2023-08-01 14:17:06 -0400

    updated db_goals.r to use single pre-2022/23 csv

--- data-raw/db_goals.R
+++ data-raw/db_goals.R
@@ -1,66 +1,17 @@
-fa_trophy_goals <- vroom::vroom(
-  file = "https://raw.githubusercontent.com/petebrown/complete-record/main/fa-trophy/fa_trophy_goals.csv",
+db_goals <- vroom::vroom(
+  file = "https://raw.githubusercontent.com/petebrown/pre-2023-data-prep/main/data/goals_df.csv",
   show_col_types = FALSE
 )
 
-sb_gf <- sb_goals %>%
-  fix_sb_player_names() %>%
-  dplyr::left_join(
-    game_ids_and_dates,
-    by = "game_id"
-  ) %>%
-  dplyr::filter(
-    goal_type == "for"
-  ) %>%
-  dplyr::select(
-    game_date,
-    player_name,
-    minute,
-    penalty,
-    own_goal
-  )
 
-goals_table <- goals %>%
-  dplyr::filter(
-    season < 1996
-  ) %>%
-  dplyr::left_join(
-    game_dates_and_nos,
-    by = c(
-      "season",
-      "game_no" = "ssn_game_no"
-    )
-  ) %>%
-  dplyr::group_by(
-    season,
-    game_no,
-    player_name
-  ) %>%
-  dplyr::slice(
-    rep(1:dplyr::n(), each = goals_scored)
-  ) %>%
-  dplyr::ungroup() %>%
-  dplyr::select(
-    game_date,
-    player_name
-  ) %>%
-  dplyr::bind_rows(
-    sb_gf
-  ) %>%
-  dplyr::mutate(
-    own_goal = dplyr::case_match(
-      player_name,
-      "OG" ~ 1,
-      .default = own_goal
-    )
-  ) %>%
-  rbind(
-    fa_trophy_goals
-  ) %>%
-  dplyr::arrange(
-    game_date,
-    minute,
-    player_name
-  )
+# Send db_goals to /data directory
 
-usethis::use_data(goals_table, overwrite = TRUE)
+usethis::use_data(db_goals, overwrite = TRUE)
+
+###########################################################
+# A tibble: 7,043 × 5                                     #
+# ------------------------------------------------------- #
+# game_date | player_name | goal_min | penalty | own_goal #
+# ------------------------------------------------------- #
+#                                                         #
+###########################################################
