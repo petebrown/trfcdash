d89d67c7ee6bc1be60c72d4951b300c9362b5415
petebrown <15838790+petebrown@users.noreply.github.com>
2023-08-21 09:50:29 -0400

    moving towards updater csv

--- data-raw/db_red_cards.R
+++ data-raw/db_red_cards.R
@@ -1,6 +1,10 @@
 red_cards <- vroom::vroom(
   file = "https://raw.githubusercontent.com/petebrown/data-updater/main/data/red_cards.csv",
   show_col_types = FALSE
-)
+) %>%
+  dplyr::group_by_all() %>%
+  dplyr::mutate(
+    red_cards = dplyr::n()
+  )
 
 usethis::use_data(red_cards, overwrite = TRUE)
