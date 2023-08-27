52d38d0bcaede668b53683f3dfb0e29cc8fd4c90
petebrown <15838790+petebrown@users.noreply.github.com>
2023-08-27 14:55:35 -0400

    Added Manager gt tables

--- R/mod_ManagersByManager.R
+++ R/mod_ManagersByManager.R
@@ -10,7 +10,38 @@
 mod_ManagersByManager_ui <- function(id){
   ns <- NS(id)
   tagList(
-    h1("Managers By Manager shit goes HERE!")
+
+    bslib::page_fluid(
+      bslib::card(
+        bslib::card_header(
+          class = "bg-dark",
+          "Overall Record"
+        ),
+        bslib::card_body(
+          gt::gt_output(ns("mgr_summary_overall"))
+        )
+      ),
+
+      bslib::card(
+        bslib::card_header(
+          class = "bg-dark",
+          "Overall Record by Competition"
+        ),
+        bslib::card_body(
+          gt::gt_output(ns("mgr_summary_by_comp"))
+        )
+      ),
+
+      bslib::card(
+        bslib::card_header(
+          class = "bg-dark",
+          "Overall Record by Season"
+        ),
+        bslib::card_body(
+          gt::gt_output(ns("mgr_summary_by_ssn"))
+        )
+      )
+    )
   )
 }
 
@@ -20,6 +51,27 @@ mod_ManagersByManager_ui <- function(id){
 mod_ManagersByManager_server <- function(id, manager_name){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+
+    output$mgr_summary_overall <- {
+      gt::render_gt(
+        expr = get_mgr_summary_overall(manager_name()),
+        width = "100%"
+      )
+    }
+
+    output$mgr_summary_by_ssn <- {
+      gt::render_gt(
+        expr = get_mgr_summary_by_ssn(manager_name()),
+        width = "100%"
+      )
+    }
+
+    output$mgr_summary_by_comp <- {
+      gt::render_gt(
+        expr = get_mgr_summary_by_comp(manager_name()),
+        width = "100%"
+      )
+    }
 
   })
 }
--- R/mod_ManagersByManager_fct_01_man_summary.R
+++ R/mod_ManagersByManager_fct_01_man_summary.R
@@ -1,10 +1,25 @@
-man_summary <- function(selected_manager) {
-  results_dataset %>%
+get_mgr_summary_by_ssn <- function(selected_manager) {
+  table <- results_dataset %>%
+    dplyr::mutate(
+      competition = dplyr::case_when(
+        league_tier == 2 & stringr::str_detect(competition, "Play-Off", negate = TRUE) ~ "Championship",
+        league_tier == 2 & stringr::str_detect(competition, "Play-Off") ~ "Championship Play-Off",
+        league_tier == 3 & stringr::str_detect(competition, "Play-Off", negate = TRUE) ~ "League One",
+        league_tier == 3 & stringr::str_detect(competition, "Play-Off") ~ "League One Play-Off",
+        league_tier == 4 & stringr::str_detect(competition, "Play-Off", negate = TRUE) ~ "League Two",
+        league_tier == 4 & stringr::str_detect(competition, "Play-Off") ~ "League Two Play-Off",
+        league_tier == 5 & stringr::str_detect(competition, "Play-Off", negate = TRUE) ~ "National League",
+        league_tier == 5 & stringr::str_detect(competition, "Play-Off") ~ "National League Play-Off",
+        .default = generic_comp
+      )
+    ) %>%
     dplyr::filter(
-      manager == selected_manager
+      manager == selected_manager,
     ) %>%
     dplyr::group_by(
-      generic_comp
+      season,
+      generic_comp,
+      competition,
     ) %>%
     dplyr::summarise(
       P = dplyr::n(),
@@ -13,7 +28,12 @@ man_summary <- function(selected_manager) {
       L = sum(outcome == "L"),
       GF = sum(goals_for),
       GA = sum(goals_against),
-      win_pc = W / P
+      w_pc = ifelse(W / P == 0, NA, W / P),
+      d_pc = ifelse(D / P == 0, NA, D / P),
+      l_pc = ifelse(L / P == 0, NA, L / P),
+      outcomes = list(c(w_pc, d_pc, l_pc)),
+      goals = list(c(GF, GA)),
+      .groups = "drop"
     ) %>%
     dplyr::mutate(
       generic_comp = factor(generic_comp, levels = c(
@@ -28,6 +48,166 @@ man_summary <- function(selected_manager) {
     dplyr::arrange(
       generic_comp
     )
+
+  table %>%
+    dplyr::mutate(
+      season = stringr::str_glue("### {season}")
+    ) %>%
+    dplyr::select(-w_pc, -d_pc, -l_pc, -goals, -generic_comp
+    ) %>%
+    gt::gt(
+      groupname_col = "season",
+      process_md = TRUE
+    ) %>%
+    gt::cols_align(
+      columns = competition,
+      align = "left"
+    ) %>%
+    gt::cols_label(
+      competition = "",
+      P = gt::md("**P**"),
+      W = gt::md("**W**"),
+      D = gt::md("**D**"),
+      L = gt::md("**L**"),
+      GF = gt::md("**GF**"),
+      GA = gt::md("**GA**")
+    ) %>%
+    gtExtras::gt_plt_bar_stack(
+      outcomes,
+      width = 150,
+      labels = c("Wins ", " Draws ", " Defeats"),
+      palette= c("darkgreen", "#999999", "#CD4F39"),
+      fmt_fn = scales::label_percent(accuracy = 1),
+    ) %>%
+    gt::tab_options(
+      table.width = "100%",
+      row_group.background.color = "grey98",
+      row_group.padding = "0.85rem"
+    )
 }
 
-man_summary("Ian Dawes")
+
+get_mgr_summary_by_comp <- function(selected_manager) {
+  table <- results_dataset %>%
+    dplyr::mutate(
+      competition = dplyr::case_when(
+        league_tier == 2 & stringr::str_detect(competition, "Play-Off", negate = TRUE) ~ "Championship",
+        league_tier == 2 & stringr::str_detect(competition, "Play-Off") ~ "Championship Play-Off",
+        league_tier == 3 & stringr::str_detect(competition, "Play-Off", negate = TRUE) ~ "League One",
+        league_tier == 3 & stringr::str_detect(competition, "Play-Off") ~ "League One Play-Off",
+        league_tier == 4 & stringr::str_detect(competition, "Play-Off", negate = TRUE) ~ "League Two",
+        league_tier == 4 & stringr::str_detect(competition, "Play-Off") ~ "League Two Play-Off",
+        league_tier == 5 & stringr::str_detect(competition, "Play-Off", negate = TRUE) ~ "National League",
+        league_tier == 5 & stringr::str_detect(competition, "Play-Off") ~ "National League Play-Off",
+        .default = generic_comp
+      )
+    ) %>%
+    dplyr::filter(
+      manager == selected_manager
+    ) %>%
+    dplyr::group_by(
+      generic_comp,
+      competition,
+    ) %>%
+    dplyr::summarise(
+      P = dplyr::n(),
+      W = sum(outcome == "W"),
+      D = sum(outcome == "D"),
+      L = sum(outcome == "L"),
+      GF = sum(goals_for),
+      GA = sum(goals_against),
+      w_pc = ifelse(W / P == 0, NA, W / P),
+      d_pc = ifelse(D / P == 0, NA, D / P),
+      l_pc = ifelse(L / P == 0, NA, L / P),
+      outcomes = list(c(w_pc, d_pc, l_pc)),
+      goals = list(c(GF, GA)),
+      .groups = "drop"
+    ) %>%
+    dplyr::mutate(
+      generic_comp = factor(generic_comp, levels = c(
+        "Football League",
+        "Non-League",
+        "FA Cup",
+        "League Cup",
+        "Associate Members' Cup"
+      )
+      )
+    ) %>%
+    dplyr::arrange(
+      generic_comp
+    )
+
+  table %>%
+    dplyr::select(-w_pc, -d_pc, -l_pc, -goals, -generic_comp
+    ) %>%
+    gt::gt() %>%
+    gt::cols_align(
+      columns = competition,
+      align = "left"
+    ) %>%
+    gt::cols_label(
+      competition = "",
+      P = gt::md("**P**"),
+      W = gt::md("**W**"),
+      D = gt::md("**D**"),
+      L = gt::md("**L**"),
+      GF = gt::md("**GF**"),
+      GA = gt::md("**GA**")
+    ) %>%
+    gtExtras::gt_plt_bar_stack(
+      outcomes,
+      width = 150,
+      labels = c("Wins ", " Draws ", " Defeats"),
+      palette= c("darkgreen", "#999999", "#CD4F39"),
+      fmt_fn = scales::label_percent(accuracy = 1)
+    ) %>%
+    gt::tab_options(
+      table.width = "100%"
+    )
+}
+
+
+get_mgr_summary_overall <- function(selected_manager) {
+  table <- results_dataset %>%
+    dplyr::filter(
+      manager == selected_manager
+    ) %>%
+    dplyr::group_by(
+      manager
+    ) %>%
+    dplyr::summarise(
+      P = dplyr::n(),
+      W = sum(outcome == "W"),
+      D = sum(outcome == "D"),
+      L = sum(outcome == "L"),
+      GF = sum(goals_for),
+      GA = sum(goals_against),
+      w_pc = ifelse(W / P == 0, NA, W / P),
+      d_pc = ifelse(D / P == 0, NA, D / P),
+      l_pc = ifelse(L / P == 0, NA, L / P),
+      outcomes = list(c(w_pc, d_pc, l_pc)),
+      .groups = "drop"
+    ) %>%
+    dplyr::select(-manager, -w_pc, -d_pc, -l_pc)
+
+  table %>%
+    gt::gt() %>%
+    gt::cols_label(
+      P = gt::md("**P**"),
+      W = gt::md("**W**"),
+      D = gt::md("**D**"),
+      L = gt::md("**L**"),
+      GF = gt::md("**GF**"),
+      GA = gt::md("**GA**")
+    ) %>%
+    gtExtras::gt_plt_bar_stack(
+      outcomes,
+      width = 150,
+      labels = c("Wins ", " Draws ", " Defeats"),
+      palette= c("darkgreen", "#999999", "#CD4F39"),
+      fmt_fn = scales::label_percent(accuracy = 1)
+    ) %>%
+    gt::tab_options(
+      table.width = "100%"
+    )
+}
