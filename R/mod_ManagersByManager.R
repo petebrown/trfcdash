25b4e52701e92e945f011fa65864e35e6c176233
petebrown <15838790+petebrown@users.noreply.github.com>
2023-08-31 10:11:54 -0400

    added manager images

--- R/mod_ManagersByManager.R
+++ R/mod_ManagersByManager.R
@@ -12,6 +12,9 @@ mod_ManagersByManager_ui <- function(id){
   tagList(
 
     bslib::page_fluid(
+
+      uiOutput(ns("manager_img")),
+
       bslib::card(
         bslib::card_header(
           class = "bg-dark",
@@ -51,6 +54,36 @@ mod_ManagersByManager_ui <- function(id){
 mod_ManagersByManager_server <- function(id, manager_name){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+
+    output$manager_img <- renderUI({
+      manager_name <- manager_name()
+
+      title <- h1(manager_name)
+
+      img_dir = "./www/images/managers"
+
+      if (stringr::str_detect(manager_name, " & ")) {
+        manager_name <- stringr::str_split_i(manager_name(), " &", 1)
+      }
+
+      file_name = paste0(
+        stringr::str_to_lower(stringr::str_replace_all(manager_name, " ", "-")),
+        ".jpg"
+      )
+
+      file_path = file.path(img_dir, file_name)
+
+      bslib::card(
+        h1(manager_name()),
+        img(
+          src = file_path,
+          class = "rounded-circle",
+          width = 150,
+          height = 150,
+          alt = manager_name
+        )
+      )
+    })
 
     output$mgr_summary_overall <- {
       gt::render_gt(
