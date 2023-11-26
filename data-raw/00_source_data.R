bb13290a4184afd8cd0b2316a77b17ef2f1aba7d
petebrown <15838790+petebrown@users.noreply.github.com>
2023-11-25 20:32:53 -0500

    updated after gillingham (h)

--- data-raw/00_source_data.R
+++ data-raw/00_source_data.R
@@ -1,56 +1,3 @@
-###########
-# RESULTS #
-###########
-
-results_dataset <- vroom::vroom(
-  file = "https://raw.githubusercontent.com/petebrown/data-updater/main/data/results.csv",
-  show_col_types = FALSE
-) %>%
-  dplyr::left_join(
-    goalscorers_by_game,
-    by = "game_date"
-  ) %>%
-  dplyr::mutate(
-    ssn_year = as.numeric(stringr::str_sub(season, end = 4)),
-    game_year = lubridate::year(game_date),
-    game_month = lubridate::month(game_date),
-    game_day = lubridate::day(game_date),
-  )
-
-
-first_game_current_season <- min(results_dataset[results_dataset$season == max(results_dataset$season), ]$game_date)
-
-game_lengths <- results_dataset %>%
-  dplyr::select(
-    game_date,
-    game_length
-  )
-
-
-season_game_dates <- results_dataset %>%
-  dplyr::select(
-    game_date,
-    season
-  )
-
-
-season_game_nos <- results_dataset %>%
-  dplyr::select(
-    game_date,
-    game_no
-  )
-
-
-usethis::use_data(
-  results_dataset,
-  game_lengths,
-  season_game_dates,
-  season_game_nos,
-
-  overwrite = TRUE
-)
-
-
 #########
 # GOALS #
 #########
@@ -148,6 +95,59 @@ usethis::use_data(
 
   overwrite = TRUE
 )
+
+###########
+# RESULTS #
+###########
+
+results_dataset <- vroom::vroom(
+  file = "https://raw.githubusercontent.com/petebrown/data-updater/main/data/results.csv",
+  show_col_types = FALSE
+) %>%
+  dplyr::left_join(
+    goalscorers_by_game,
+    by = "game_date"
+  ) %>%
+  dplyr::mutate(
+    ssn_year = as.numeric(stringr::str_sub(season, end = 4)),
+    game_year = lubridate::year(game_date),
+    game_month = lubridate::month(game_date),
+    game_day = lubridate::day(game_date),
+  )
+
+
+first_game_current_season <- min(results_dataset[results_dataset$season == max(results_dataset$season), ]$game_date)
+
+game_lengths <- results_dataset %>%
+  dplyr::select(
+    game_date,
+    game_length
+  )
+
+
+season_game_dates <- results_dataset %>%
+  dplyr::select(
+    game_date,
+    season
+  )
+
+
+season_game_nos <- results_dataset %>%
+  dplyr::select(
+    game_date,
+    game_no
+  )
+
+
+usethis::use_data(
+  results_dataset,
+  game_lengths,
+  season_game_dates,
+  season_game_nos,
+
+  overwrite = TRUE
+)
+
 
 #############
 # RED CARDS #
